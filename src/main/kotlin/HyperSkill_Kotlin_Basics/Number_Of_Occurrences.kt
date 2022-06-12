a919209ca28eb9de6d2d551efcbdc9a54a20fab2
SpencerDamon <spencerdamon1974@gmail.com>
2022-06-12 13:05:13 -0700

    Create Package HyperSkill Kotlin Basics
    Add Number_Of_Occurrences.kt

---
+++ src/main/kotlin/HyperSkill_Kotlin_Basics/Number_Of_Occurrences.kt
@@ -0,0 +1,35 @@
+package HyperSkill_Kotlin_Basics
+/*
+Number of occurrences
+Write a program that finds the frequency of occurrences of substring in given string.
+
+
+Input data format
+
+The first input line contains a string, the second one contains a substring.
+
+
+Sample Input:
+hello there
+the
+
+Sample Output:
+1
+*/
+
+fun main() {
+    // First Attempt - Readability
+    val input1 = readln()
+    val target1 = readln()
+    val occurrences1 = input1.split("$target1").toMutableList()
+    println(occurrences1.lastIndex)
+
+    // Second Attempt - Density
+/*fun main() = */ println(readln().split("${readln()}").lastIndex)
+}
+
+/*
+Get input
+Split the input with the target to a new list
+Return the last index of the new list
+ */

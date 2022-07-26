e37ff159f8e9bc6fa0f6bd5ff2ad9fa2a3109d73
SpencerDamon <spencerdamon1974@gmail.com>
2022-07-26 10:45:27 -0700

    Add Topic toString() solve problem Latte

---
+++ src/main/kotlin/HyperSkill_Kotlin_Basics/toString()_Latte.kt
@@ -0,0 +1,19 @@
+package HyperSkill_Kotlin_Basics
+
+/*
+Latte
+The class Latte is inherited from the class Coffee. Override the toString() function so that
+it returns the same as for the parent class. Add the information about milk like in the template
+at the end: SUPER; milk=MILK, where MILK is the value of the parameter milk and SUPER is
+superclass realization.
+
+Sample output:
+
+SUPER; milk=almond
+ */
+
+class Latte(val milk: String, cost: Int, volume: Int) : Coffee(cost, volume) {
+    override fun toString(): String {
+        return "${super.toString()}; milk=$milk"
+    }
+}
